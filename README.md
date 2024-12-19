d4d0bd02efe0a40125a2473a120a35f9de2cfa83
Charly Huang <charly.charlongo@gmail.com>
2024-12-19 18:05:54 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Introduction
+
+This is a projected directly ported from [ndt_matching repository](https://github.com/atokuz/ndt_matching/tree/master) as I want to make sure that the code is ROS2-independent such that it can be easily integrated into my ROS2 project following the Open-Close principle.
+
+## Getting started
+
+### Dependencies
+
+- PCL
+
+### Limitations
+
+
+## Development roadmap
---
+++ include/ndt_matching/common_includes.h
@@ -0,0 +1,27 @@
+#ifndef _COMMON_INCLUDES_H_
+#define _COMMON_INCLUDES_H_
+
+#include <cstdio>
+#include <memory>
+#include <string>
+#include <iostream>
+#include <float.h>
+#include <vector>
+#include <memory>
+#include <sys/time.h>
+#include <math.h>
+#include <cmath>
+#include <limits>
+#include <bitset>
+#include <inttypes.h>
+#include <chrono>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/common/transforms.h>
+
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
+
+#endif /* _COMMON_INCLUDES_H_ */
